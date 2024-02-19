40e67608fa70fad50b76e8a3bf5cd17e937ee98e
tr7zw <tr7zw@live.de>
2024-02-19 15:59:39 +0100

    Switch to new compose structure

---
+++ src/main/java/dev/tr7zw/disguiseheads/DisguiseHeadsMod.java
@@ -0,0 +1,11 @@
+package dev.tr7zw.disguiseheads;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DisguiseHeadsMod extends DisguiseHeadsShared implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        init();
+    }
+}
