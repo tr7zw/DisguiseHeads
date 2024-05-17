bcb899f620c98f59a9855e8a45c327d3e10e3f09
tr7zw <tr7zw@live.de>
2024-05-17 19:35:22 +0200

    Turn off setting that was supposed to be off by default

--- src/main/java/dev/tr7zw/disguiseheads/config/Config.java
+++ src/main/java/dev/tr7zw/disguiseheads/config/Config.java
@@ -3,7 +3,7 @@ package dev.tr7zw.disguiseheads.config;
 public class Config {
 
     public boolean enablePlayerDisguise = true;
-    public boolean enableArmorstandDisguise = true;
+    public boolean enableArmorstandDisguise = false;
     public boolean hideArmorstandHead = false;
     public boolean changeNameToDisguise = false;
 
