6bef42b9e230cd5b782da5f149aba5b488a5219c
tr7zw <tr7zw@live.de>
2025-08-13 20:47:06 +0200

    Probably fix Forge/NeoForge

--- src/main/java/dev/tr7zw/disguiseheads/DisguiseHeadsBootstrap.java
+++ src/main/java/dev/tr7zw/disguiseheads/DisguiseHeadsBootstrap.java
@@ -4,30 +4,36 @@
 //$$import net.minecraftforge.api.distmarker.Dist;
 //$$import net.minecraftforge.fml.DistExecutor;
 //$$import net.minecraftforge.fml.common.Mod;
+//$$import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+//$$import dev.tr7zw.transition.loader.ModLoaderUtil;
 //$$
 //$$@Mod("disguiseheads")
 //$$public class DisguiseHeadsBootstrap {
 //$$
-//$$	public DisguiseHeadsBootstrap() {
+//$$	public DisguiseHeadsBootstrap(FMLJavaModLoadingContext context) {
+//$$        ModLoaderUtil.setModLoadingContext(context);
 //$$		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> { 
 //$$         new DisguiseHeadsShared().init();
 //$$        });
 //$$	}
+//$$    public DisguiseHeadsBootstrap() {
+//$$        this(FMLJavaModLoadingContext.get());
+//$$    }
 //$$	
 //$$}
 //#elseif NEOFORGE
 //$$package dev.tr7zw.disguiseheads;
 //$$
-//$$import net.neoforged.api.distmarker.Dist;
 //$$import net.neoforged.fml.loading.FMLEnvironment;
 //$$import net.neoforged.fml.common.Mod;
+//$$import dev.tr7zw.transition.loader.ModLoaderEventUtil;
 //$$
 //$$@Mod("disguiseheads")
 //$$public class DisguiseHeadsBootstrap {
 //$$
 //$$    public DisguiseHeadsBootstrap() {
-//$$            if(FMLEnvironment.dist == Dist.CLIENT) {
-//$$                new DisguiseHeadsShared().init();
+//$$            if (FMLEnvironment.dist.isClient()){
+//$$                    ModLoaderEventUtil.registerClientSetupListener(() -> new DisguiseHeadsShared().init());
 //$$            }
 //$$    }
 //$$
